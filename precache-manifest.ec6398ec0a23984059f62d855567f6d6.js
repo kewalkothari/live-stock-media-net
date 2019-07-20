dc1803d60c4defc59384c4119ec93371f4b51f48
Kewal Kothari <kotharikewal7@gmail.com>
2019-07-20 16:39:54 +0530

    Updates

---
+++ precache-manifest.ec6398ec0a23984059f62d855567f6d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2be0da54a6c915c01f8d6b8bfd1f315",
+    "url": "/live-stock-media-net/index.html"
+  },
+  {
+    "revision": "907b20fd7b2dc25d86e0",
+    "url": "/live-stock-media-net/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "076189260de17ab4e29b",
+    "url": "/live-stock-media-net/static/js/2.bec66cbf.chunk.js"
+  },
+  {
+    "revision": "907b20fd7b2dc25d86e0",
+    "url": "/live-stock-media-net/static/js/main.e6a8ee18.chunk.js"
+  },
+  {
+    "revision": "5583d16eb1bb36534654",
+    "url": "/live-stock-media-net/static/js/runtime~main.c67c4a5b.js"
+  }
+]);
