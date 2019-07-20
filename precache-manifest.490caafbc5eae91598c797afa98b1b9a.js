058bb046b3dc216fa89ee2f3e60243aae82400d9
kewalkothari <kewalkothari@users.noreply.github.com>
2019-07-20 15:43:02 +0000

    Updates

--- precache-manifest.490caafbc5eae91598c797afa98b1b9a.js
+++ precache-manifest.490caafbc5eae91598c797afa98b1b9a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a33cf2b74eb4ec8e041a53014d6282f7",
+    "revision": "4a15477dfecf9af41ca71a929f21d4ed",
     "url": "/live-stock-media-net/index.html"
   },
   {
-    "revision": "85769100f478bf78de88",
+    "revision": "dc995e210e8d8b9c6127",
     "url": "/live-stock-media-net/static/css/main.f689211e.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/live-stock-media-net/static/js/2.6d889f34.chunk.js"
   },
   {
-    "revision": "85769100f478bf78de88",
-    "url": "/live-stock-media-net/static/js/main.8c3924c3.chunk.js"
+    "revision": "dc995e210e8d8b9c6127",
+    "url": "/live-stock-media-net/static/js/main.8b906d9a.chunk.js"
   },
   {
     "revision": "5583d16eb1bb36534654",
